a57c810ae94beeaaa1e20a19dd8a35607dfd9da3
GaborThury <thury.gabor1994@gmail.com>
2025-06-05 20:42:37 +0200

    add additional chrome options

--- src/test/java/SubpageBase.java
+++ src/test/java/SubpageBase.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.selenium.helper.config.Config;
 
 import java.util.concurrent.Semaphore;
@@ -27,7 +28,14 @@ public abstract class SubpageBase implements Subpage {
     @BeforeEach
     public void setUp() throws InterruptedException {
         semaphore.acquire();
-        driver = new ChromeDriver();
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-gpu");
+
+        driver = new ChromeDriver(options);
         driver.get(Config.get("baseurl"));
         driver.manage().window().maximize();
         driver.findElement(By.linkText(getLinkText())).click();
